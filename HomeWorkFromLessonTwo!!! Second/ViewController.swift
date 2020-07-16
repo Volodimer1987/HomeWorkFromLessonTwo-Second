10e93e22cf3bd77046c8ef1df602d0e3f39c3242
Vladimir <kynicin87@mail.ru>
2020-07-16 11:58:52 +0300

    all sliders colors is ready

--- HomeWorkFromLessonTwo!!! Second/ViewController.swift
+++ HomeWorkFromLessonTwo!!! Second/ViewController.swift
@@ -1,20 +1,63 @@
-//
-//  ViewController.swift
-//  HomeWorkFromLessonTwo!!! Second
-//
-//  Created by vladimir gennadievich on 15.07.2020.
-//  Copyright © 2020 Vladimir Gennadievich. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var viewOfMixedColors: UIView!
+    
+    @IBOutlet var redStringColorLabel: UILabel!
+    @IBOutlet var greenStringColorLabel: UILabel!
+    @IBOutlet var blueStringColorLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var labelShowResultRed: UILabel!
+    @IBOutlet var labelShowResultGreen: UILabel!
+    @IBOutlet var labelShowResultBlue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redStringColorLabel.backgroundColor = view.backgroundColor
+        greenStringColorLabel.backgroundColor = view.backgroundColor
+        blueStringColorLabel.backgroundColor = view.backgroundColor
+        
+        redSlider.value = 0.5
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        
+        greenSlider.value = 0.5
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        
+        blueSlider.value = 0.5
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        
+        redSlider.minimumTrackTintColor = .red
+        redSlider.maximumTrackTintColor = .systemGray6
+        
+        greenSlider.minimumTrackTintColor = .green
+        greenSlider.maximumTrackTintColor = .systemGray6
+        
+        blueSlider.minimumTrackTintColor = .blue
+        blueSlider.maximumTrackTintColor = .systemGray6
+
+
     }
 
-
+    @IBAction func redLabelActionSlider() {
+        
+    }
+    @IBAction func greenLabelActionSlider() {
+        
+    }
+    
+    @IBAction func blueLabelActionSlider() {
+        
+    }
+    
 }
 
